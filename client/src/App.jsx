446a2a2cd5b6fbb5a796c276f3cce22bea6fa97f
Bakul009 <bakulsarkar007@gmail.com>
2024-01-25 03:20:45 +0530

    pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Signin from './pages/Signin'
+import Project from './pages/Project'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 export default function App() {
   return (
     <div>
-      webown
+     <BrowserRouter>
+     <Header/>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/project" element={<Project/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   )
 }
